37f61c210ce67434dbb708401c774237e88d29ea
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2021-01-29 12:43:12 -0500

    Remove script source

--- footer.php
+++ footer.php
@@ -35,11 +35,6 @@
   </div>
 </footer>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.js"></script>
-<!-- or -->
-<script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js"></script>
-
 <?php wp_footer(); ?>
 
 </body>
