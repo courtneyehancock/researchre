86a5c2b122ca5b872908d7e4d07da210ee05f975
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2021-02-03 10:13:21 -0500

    Add Bootstrap.js to functions

--- functions.php
+++ functions.php
@@ -22,6 +22,8 @@
   //Javascript files
     wp_enqueue_script('custom-js', get_template_directory_uri() . '/js/main.js');
     wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js');
+    wp_enqueue_script('bootstrap-jq', get_template_directory_uri() . '/js/bootstrap.js');
+
   }
 
   add_action('wp_enqueue_scripts', 'custom_theme_scripts');
