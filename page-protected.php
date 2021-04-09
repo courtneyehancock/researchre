80502fe284f89920534aab9dd17b9e9e43fd0fcc
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2021-04-09 11:47:08 -0400

    Add page title to protected page

--- page-protected.php
+++ page-protected.php
@@ -3,6 +3,9 @@
 <?php get_header(); ?>
 
 <div class="main-content internal-div">
+  <h1 style="color: #00529b; font-size: 2em; text-align: right; margin-right: 2em;">ATTENDEE PROFILES</h1>
+
+<hr />
 <div class="container">
         <?php
           if(have_posts()){
