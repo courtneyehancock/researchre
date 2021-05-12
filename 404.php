171ad80117e7750b2809695f3cf59d366616634c
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2021-05-12 12:29:49 -0400

    Edit conference client 404 pages

--- 404.php
+++ 404.php
@@ -1,14 +1,14 @@
 <?php /*Template Name: 404 Page*/?>
 <?php get_header(); ?>
 
-<div class="container">
-<div class="row">
-  <div class="col-lg-12 not-found">
-    <h2>Page Not Found</h2>
-    <h3>This usually never happens.</h3>
-    <p>It looks like nothing was found at this location. Would you like to go back <a href="<?php echo home_url('/');?>">home</a></p>
+<div class="container mt-5 mb-5">
+  <div class="not-found">
+    <img src="https://pwd.aa.ufl.edu/wp-content/uploads/2021/05/Telescope-Icon-404.png" class="img-fluid" style="max-width:300px;"/>
+
+    <h1 class="mt-4">OOPS! PAGE NOT FOUND</h1>
+    <h2>This usually never happens.</h2>
+    <p>It looks like nothing was found at this location. Would you like to go back <a href="<?php echo home_url('/');?>">home</a>?</p>
   </div>
-</div>
 </div>
 
 <?php get_footer(); ?>
