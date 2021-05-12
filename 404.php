bbd33209d656ca12673035f79c883b49f00561c9
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2021-05-12 12:30:43 -0400

    Edit 404 padding

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
 
 <div class="container mt-5 mb-5">
   <div class="not-found">
-    <img src="https://pwd.aa.ufl.edu/wp-content/uploads/2021/05/Telescope-Icon-404.png" class="img-fluid" style="max-width:300px;"/>
+    <img src="https://pwd.aa.ufl.edu/wp-content/uploads/2021/05/Telescope-Icon-404.png" class="img-fluid mt-5 pt-5" style="max-width:300px;"/>
 
     <h1 class="mt-4">OOPS! PAGE NOT FOUND</h1>
     <h2>This usually never happens.</h2>
