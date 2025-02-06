2308b29ba5524a0c633700b08e37d10e9e714802
Courtney Glancy <42854186+courtneyehancock@users.noreply.github.com>
2025-02-06 14:17:39 -0500

    Add breadcrumbs code to internal template

--- page-internal.php
+++ page-internal.php
@@ -1,7 +1,15 @@
 <?php /*Template Name: Internal Page Template*/ ?>
 
 <?php get_header(); ?>
-
+<div class="breadcrumbs-container">
+<div class="container">
+<?php
+if ( function_exists('yoast_breadcrumb') ) {
+  yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
+}
+?>
+</div>
+</div>
 <div class="main-content internal-div">
 
         <?php
